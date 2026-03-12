123cd942cb5a2f3de0b9c8a6cce1bc11b4654e86
Montse Jiménez <mjimenez9708@gmail.com>
2026-03-11 22:15:39 -0600

    Creación de html, css, y servidor

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
