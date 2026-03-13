48b9e8b4c0bb2f53bba3b9939eebe8f47ed70920
Montse Jiménez <mjimenez9708@gmail.com>
2026-03-12 19:59:10 -0600

    agregué el div de tareas y modifiqué formulario en html, agregué estilos a ambos, y añadí preventDefault al formulario en js

---
+++ script.js
@@ -0,0 +1,7 @@
+const formulario = document.getElementById("formulario");
+const boton = document.getElementById("btn");
+
+formulario.addEventListener("submit", function (event){
+    event.preventDefault();
+});
+
